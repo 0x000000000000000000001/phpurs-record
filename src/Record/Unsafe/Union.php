9f958336ede30cee273e236f33ce9c9d20cbfc20
0x1 <kevin.francart3@gmail.com>
2026-07-08 16:49:27 +0200

    Add PHP FFI for Unsafe.Union

---
+++ src/Record/Unsafe/Union.php
@@ -0,0 +1,31 @@
+<?php
+
+$_unsafeUnionFn = function($r1, $r2) {
+    $copy = [];
+    
+    if (is_object($r2)) {
+        foreach (get_object_vars($r2) as $k => $v) {
+            $copy[$k] = $v;
+        }
+    } elseif (is_array($r2)) {
+        foreach ($r2 as $k => $v) {
+            $copy[$k] = $v;
+        }
+    }
+
+    if (is_object($r1)) {
+        foreach (get_object_vars($r1) as $k => $v) {
+            $copy[$k] = $v;
+        }
+    } elseif (is_array($r1)) {
+        foreach ($r1 as $k => $v) {
+            $copy[$k] = $v;
+        }
+    }
+    
+    return (object)$copy;
+};
+
+$exports['unsafeUnionFn'] = $_unsafeUnionFn;
+
+return $exports;
