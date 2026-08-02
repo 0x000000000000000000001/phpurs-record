38bbc57ab9fd7927e97605f02667329c68b83c04
0x1 <kevin.francart3@gmail.com>
2026-08-02 13:15:53 +0200

    wip

--- src/Record/Builder.php
+++ src/Record/Builder.php
@@ -10,42 +10,42 @@ $_copyRecord = function($rec) {
 };
 
 $_unsafeInsert = function($l, $a, $rec) {
-            if (\is_array($rec)) {
-                $rec[$l] = $a;
-            } else {
-                $rec->{$l} = $a;
-            }
-            return $rec;
-        };
+    if (\is_array($rec)) {
+        $rec[$l] = $a;
+    } else {
+        $rec->{$l} = $a;
+    }
+    return $rec;
+};
 
 $_unsafeModify = function($l, $f, $rec) {
-            if (\is_array($rec)) {
-                $rec[$l] = $f($rec[$l]);
-            } else {
-                $rec->{$l} = $f($rec->{$l});
-            }
-            return $rec;
-        };
+    if (\is_array($rec)) {
+        $rec[$l] = $f($rec[$l]);
+    } else {
+        $rec->{$l} = $f($rec->{$l});
+    }
+    return $rec;
+};
 
 $_unsafeDelete = function($l, $rec) {
-        if (\is_array($rec)) {
-            unset($rec[$l]);
-        } else {
-            unset($rec->{$l});
-        }
-        return $rec;
-    };
+    if (\is_array($rec)) {
+        unset($rec[$l]);
+    } else {
+        unset($rec->{$l});
+    }
+    return $rec;
+};
 
 $_unsafeRename = function($l1, $l2, $rec) {
-            if (\is_array($rec)) {
-                $rec[$l2] = $rec[$l1];
-                unset($rec[$l1]);
-            } else {
-                $rec->{$l2} = $rec->{$l1};
-                unset($rec->{$l1});
-            }
-            return $rec;
-        };
+    if (\is_array($rec)) {
+        $rec[$l2] = $rec[$l1];
+        unset($rec[$l1]);
+    } else {
+        $rec->{$l2} = $rec->{$l1};
+        unset($rec->{$l1});
+    }
+    return $rec;
+};
 
 $exports['copyRecord'] = $_copyRecord;
 $exports['unsafeInsert'] = $_unsafeInsert;
